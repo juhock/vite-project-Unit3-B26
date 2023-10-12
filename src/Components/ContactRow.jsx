c1d8e66451a7dbbd77389e4a5f8ca25e14a55bf0
Julia Hocken <juliahocken@Julias-MacBook-Pro-2.local>
2023-10-12 14:14:35 -0500

    contact row components added

--- src/Components/ContactRow.jsx
+++ src/Components/ContactRow.jsx
@@ -1,7 +1,13 @@
 
 
-export default ContactRow() {
+export default function ContactRow( {contact} ) {
 
-
+return (
+    <tr>
+        <td>{contact.name}</td>
+        <td>{contact.email}</td>
+        <td>{contact.phone}</td>
+    </tr>
+)
     
 }
